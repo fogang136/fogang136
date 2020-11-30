c946c9167751bd2d5f9b9712cb7c007a33b1b927
fogang136 <fogang136@gmail.com>
2020-11-30 18:46:05 +0800

    Update qtv.php

--- qtv.php
+++ qtv.php
@@ -4,95 +4,23 @@
 
 广东频道,#genre#
 广东珠江,http://fogang136.cf/daili/gdstv.php?id=zjpd 
-广东珠江,http://fogang136.cf/daili/gudoutv.php?id=gdzhujiang_1500
+
 广东公共,http://fogang136.cf/daili/gdstv.php?id=ggpd
-广东公共,http://fogang136.cf/daili/gudoutv.php?id=gdpublic_1500
+
 广东新闻,http://fogang136.cf/daili/gdstv.php?id=xwpd
-广东新闻,http://fogang136.cf/daili/gudoutv.php?id=gdnews_1500
-佛冈电视台,http://fogang136.cf/daili/touchtv.php?id=2460
+
+
 清新公共频道,http://hls.wiseqx.com/live/qxgg.m3u8
 清新综合频道,http://hls.wiseqx.com/live/qxzh.m3u8
-清远综合,http://fogang136.cf/daili/qytv.php?id=qyzh
+
 清远综合,http://stream1.0763f.com/qyzh/sd/live.m3u8
 清远综合,http://dslive.grtn.cn/qyzh/sd/live.m3u8
-广东南方卫视,http://fogang136.cf/daili/gdxx.php?id=tvs2
-广州新闻频道,http://fogang136.cf/daili/gdgz.php?id=gzxw
-广州新闻频道,http://fogang136.cf/daili/hktvdl.php?id=11
-广州影视频道,http://fogang136.cf/daili/gdgz.php?id=gzys
-广州综合频道,http://fogang136.cf/daili/gdgz.php?id=gzzh
-广州法治频道,http://fogang136.cf/daili/gdgz.php?id=gzfz
-广州生活频道,http://fogang136.cf/daili/gdgz.php?id=gzsh
-广州竞赛频道,http://fogang136.cf/daili/gdgz.php?id=gzjs
-南方卫视,http://fogang136.cf/daili/gudoutv.php?id=gdnanfang_1500
-广东经视,http://fogang136.cf/daili/gudoutv.php?id=GDjingjikejiao_3000
-广东综艺,http://fogang136.cf/daili/gudoutv.php?id=gdzongyi_1500
-广东影视,http://fogang136.cf/daili/gudoutv.php?id=gdyingshi_1500
-广东少儿,http://fogang136.cf/daili/gudoutv.php?id=gdshaoer_1500
-广东房产,http://fogang136.cf/daili/gudoutv.php?id=fangchan_1500
-珠江电影,http://fogang136.cf/daili/gudoutv.php?id=zjfilms_1500
-现代教育,http://fogang136.cf/daili/gudoutv.php?id=xiandaijiaoyu_1500
-岭南戏曲,http://fogang136.cf/daili/gudoutv.php?id=lingnanxiqu_1500
-广州综合,http://fogang136.cf/daili/gudoutv.php?id=guangzhou_1500
-广州新闻,http://fogang136.cf/daili/gudoutv.php?id=gznews_1500
-广州经济,http://fogang136.cf/daili/gudoutv.php?id=gzeconomic_1500
-广州生活,http://fogang136.cf/daili/gudoutv.php?id=gzlife_1500
-广州竞赛,http://fogang136.cf/daili/gudoutv.php?id=gzcompetition_1500
-广州影视,http://fogang136.cf/daili/gudoutv.php?id=gzfilms_1500
-广州少儿,http://fogang136.cf/daili/gudoutv.php?id=gzchild_1500
-南海综合,http://fogang136.cf/daili/gudoutv.php?id=nanhaitai_2000
-顺德综合,http://fogang136.cf/daili/gudoutv.php?id=shundetai_2000
-新会综合,http://fogang136.cf/daili/gudoutv.php?id=xinhui_1500
-佛山综合,http://fogang136.cf/daili/gudoutv.php?id=foshannews_1500
-佛山公共,http://fogang136.cf/daili/gudoutv.php?id=foshanyingshi_1500
-湛江综合,http://fogang136.cf/daili/gudoutv.php?id=zhanjiangzonghe_1500
-湛江公共,http://fogang136.cf/daili/gudoutv.php?id=zhanjianggongong_1500
-韶关综合,http://fogang136.cf/daili/gudoutv.php?id=shaoguanzonghe_1500
-韶关公共,http://fogang136.cf/daili/gudoutv.php?id=shaoguangonggong_1500
-博罗经典,http://fogang136.cf/daili/gudoutv.php?id=boluojingdian_1500
-博罗综合,http://fogang136.cf/daili/gudoutv.php?id=boluozonghe_1500
-云浮一套,http://fogang136.cf/daili/gudoutv.php?id=yunfuyitao_1500
-云浮二套,http://fogang136.cf/daili/gudoutv.php?id=yunfuertao_1500
-珠海一套,http://fogang136.cf/daili/gudoutv.php?id=zhuhaiyitao_1500
-珠海二套,http://fogang136.cf/daili/gudoutv.php?id=zhuhaiertao_1500
-潮州一套,http://fogang136.cf/daili/gudoutv.php?id=chaozhouyitao_1500
-潮州二套,http://fogang136.cf/daili/gudoutv.php?id=chaozhouertao_1500
-东莞一套,http://fogang136.cf/daili/gudoutv.php?id=dongguanyitao_1500
-东莞二套,http://fogang136.cf/daili/gudoutv.php?id=dongguanertao_1500
-梅州一套,http://fogang136.cf/daili/gudoutv.php?id=meizhouyitao_1500
-梅州二套,http://fogang136.cf/daili/gudoutv.php?id=meizhouertao_1500
-江门一套,http://fogang136.cf/daili/gudoutv.php?id=jiangmenyitao_1500
-江门二套,http://fogang136.cf/daili/gudoutv.php?id=jiangmenertao_1500
-中山一套,http://fogang136.cf/daili/gudoutv.php?id=zhongshanyitao_1500
-中山二套,http://fogang136.cf/daili/gudoutv.php?id=zhongshanertao_1500
-河源一套,http://fogang136.cf/daili/gudoutv.php?id=heyuanyitao_1500
-河源二套,http://fogang136.cf/daili/gudoutv.php?id=heyuanertao_1500
-肇庆一套,http://fogang136.cf/daili/gudoutv.php?id=zhaoqingyitao_1500
-肇庆二套,http://fogang136.cf/daili/gudoutv.php?id=zhaoqingertao_1500
-惠州一套,http://fogang136.cf/daili/gudoutv.php?id=huizhouyitao_1500
-惠州二套,http://fogang136.cf/daili/gudoutv.php?id=huizhouertao_1500
-汕头一套,http://fogang136.cf/daili/gudoutv.php?id=shantouyitaogaoqing_1500
-汕头三套,http://fogang136.cf/daili/gudoutv.php?id=shantousantaogaoqing_1500
+
+
 
 
 央视高清,#genre#
-CCTV-1,http://fogang136.cf/daili/woniu.php?id=10000001
-CCTV-2,http://fogang136.cf/daili/woniu.php?id=10000002
-CCTV-3,http://fogang136.cf/daili/woniu.php?id=10000003
-CCTV-4,http://fogang136.cf/daili/woniu.php?id=10000004
-CCTV-5,http://fogang136.cf/daili/woniu.php?id=10000006
-CCTV-5+,http://fogang136.cf/daili/woniu.php?id=10000005
-CCTV-6,http://fogang136.cf/daili/woniu.php?id=10000007
-CCTV-7,http://fogang136.cf/daili/woniu.php?id=10000008
-CCTV-8,http://fogang136.cf/daili/woniu.php?id=10000009
-CCTV-9,http://fogang136.cf/daili/woniu.php?id=10000010
-CCTV-10,http://fogang136.cf/daili/woniu.php?id=10000011
-CCTV-11,http://fogang136.cf/daili/woniu.php?id=10000060
-CCTV-12,http://fogang136.cf/daili/woniu.php?id=10000063
-CCTV-13,http://fogang136.cf/daili/woniu.php?id=10000061
-CCTV-14,http://fogang136.cf/daili/woniu.php?id=10000064
-CCTV-15,http://fogang136.cf/daili/woniu.php?id=10000062
-CCTV-17,http://fogang136.cf/daili/woniu.php?id=10000065
-CETV1,http://http://fogang136.cf/daili/gudoutv.php?id=CETV-1_1500
+
 CCTV1,http://121.31.30.87:8085/ysten-business/qlive/cctv-1/1.m3u8#http://scgctvshow.sctv.com/hdqlive/cctv1/1.m3u8#http://39.134.66.66/PLTV/88888888/224/3221225601/index.m3u8#http://221.6.85.150:9000/qlive/cctv1_800/cctv1_800.m3u8
 CCTV2,http://101.71.255.229:6610/zjhs/2/10082/index.m3u8?virtualDomain=zjhs.qlive_hls.zte.com#http://221.6.85.150:9000/qlive/cctv2_800/cctv2_800.m3u8#http://hwltc.tv.cdn.zj.chinamobile.com/PLTV/88888888/224/3221228482/index.m3u8?fmt=ts2hls
 CCTV3,http://121.31.30.87:8085/ysten-business/qlive/cctv-3/1.m3u8#http://111.63.117.13:6060/030000001000/CCTV-3/CCTV-3.m3u8#http://39.134.66.66/PLTV/88888888/224/3221225490/index.m3u8
@@ -186,33 +114,6 @@ CCTV17,http://ottrrs.hl.chinamobile.com/PLTV/88888888/224/3221225765/index.m3u8#
 康巴卫视,http://223.110.247.161:6610/gitv/live1/G_KANGBA/G_KANGBA
 康巴卫视,http://scgctvshow.sctv.com/sdlive/kangba/1.m3u8
 
-综合频道,#genre#
-全纪实,http://fogang136.cf/daili/gudoutv.php?id=quanjishi_1500
-新动漫,http://fogang136.cf/daili/gudoutv.php?id=xindongman_1500
-幸福彩,http://fogang136.cf/daili/gudoutv.php?id=xingfucai_1500
-高尔夫,http://fogang136.cf/daili/gudoutv.php?id=golf_1500
-劲爆体育,http://fogang136.cf/daili/gudoutv.php?id=jinbaotiyu_1500
-欢笑剧场,http://fogang136.cf/daili/gudoutv.php?id=huanxiaojuchang_1500
-法治天地,http://fogang136.cf/daili/gudoutv.php?id=fazhitiandi_1500
-都市剧场,http://fogang136.cf/daili/gudoutv.php?id=dushijuchang_1500
-极速汽车,http://fogang136.cf/daili/gudoutv.php?id=jisuqiche_1500
-魅力音乐,http://fogang136.cf/daili/gudoutv.php?id=meiliyinyue_1500
-七彩戏剧,http://fogang136.cf/daili/gudoutv.php?id=qicaixiju_1500
-金色频道,http://fogang136.cf/daili/gudoutv.php?id=jinsepindao_1500
-生活时尚,http://fogang136.cf/daili/gudoutv.php?id=shenghuoshishang_1500
-东方财*,http://fogang136.cf/daili/gudoutv.php?id=dongfangcaijing_1500
-环球奇观,http://fogang136.cf/daili/gudoutv.php?id=huanqiuqiguan_1500
-中华美食,http://fogang136.cf/daili/gudoutv.php?id=zhonghuameishi_1500
-书画频道,http://fogang136.cf/daili/gudoutv.php?id=shuhua_1500
-网络军旗扑克牌,http://fogang136.cf/daili/gudoutv.php?id=wangluoqipai_1500
-英语辅导,http://fogang136.cf/daili/gudoutv.php?id=yingyufudao_1500
-先锋乒羽,http://fogang136.cf/daili/gudoutv.php?id=pingyu_1500
-山东教育,http://fogang136.cf/daili/gudoutv.php?id=CETV-4_1500
-中国气象,http://fogang136.cf/daili/gudoutv.php?id=zgqixiang_1500
-动漫秀场,http://fogang136.cf/daili/gudoutv.php?id=dongmanxiuchang_1500
-嘉佳卡通,http://fogang136.cf/daili/gudoutv.php?id=gdjiajiacartoon_1500
-优漫卡通,http://fogang136.cf/daili/gudoutv.php?id=youmankatong_1500
-金鹰卡通,http://fogang136.cf/daili/gudoutv.php?id=jinyingkatong_1500
 
 电影综合,#genre#
 卫视卡式台,http://45.11.2.64/ub.php?id=iptvwsks
@@ -280,136 +181,6 @@ DOX新艺,http://39.135.9.77:80/gz.ucdn.10086.cn/PLTV/88888888/224/3221226017/1/
 DOX雅趣,http://39.135.9.77:80/gz.ucdn.10086.cn/PLTV/88888888/224/3221226042/1/index.m3u8?fmt=ts2hls
 DOX怡家,http://39.135.9.77:80/gz.ucdn.10086.cn/PLTV/88888888/224/3221226016/1/index.m3u8?fmt=ts2hls
 
-YY轮播,#genre#
-Y Y 1,http://fogang136.cf/daili/yy.php?id=29900720
-Y Y 2,http://fogang136.cf/daili/yy.php?id=37758710
-Y Y 3,http://fogang136.cf/daili/yy.php?id=1353881242
-Y Y 4,http://fogang136.cf/daili/yy.php?id=14503914
-Y Y 5,http://fogang136.cf/daili/yy.php?id=32110908
-Y Y 6,http://fogang136.cf/daili/yy.php?id=38525272
-Y Y 7,http://fogang136.cf/daili/yy.php?id=34811100
-Y Y 8,http://fogang136.cf/daili/yy.php?id=1351755386
-Y Y 9,http://fogang136.cf/daili/yy.php?id=1353074059
-Y Y 10,http://fogang136.cf/daili/yy.php?id=1353507954
-Y Y 11,http://fogang136.cf/daili/yy.php?id=77869868
-Y Y 12,http://fogang136.cf/daili/yy.php?id=1352904942
-Y Y 13,http://fogang136.cf/daili/yy.php?id=1352843113
-Y Y 14,http://fogang136.cf/daili/yy.php?id=1420843376
-Y Y 15,http://fogang136.cf/daili/yy.php?id=31100550
-Y Y 16,http://fogang136.cf/daili/yy.php?id=28751126
-Y Y 17,http://fogang136.cf/daili/yy.php?id=24066336
-Y Y 18,http://fogang136.cf/daili/yy.php?id=1352227153
-Y Y 19,http://fogang136.cf/daili/yy.php?id=39589923
-Y Y 20,http://fogang136.cf/daili/yy.php?id=1382658857
-Y Y 21,http://fogang136.cf/daili/yy.php?id=28996058
-Y Y 22,http://fogang136.cf/daili/yy.php?id=1382646395
-Y Y 23,http://fogang136.cf/daili/yy.php?id=1351814644
-Y Y 24,http://fogang136.cf/daili/yy.php?id=1352952473
-Y Y 25,http://fogang136.cf/daili/yy.php?id=26650882
-Y Y 26,http://fogang136.cf/daili/yy.php?id=28265277
-Y Y 27,http://fogang136.cf/daili/yy.php?id=33519757
-Y Y 28,http://fogang136.cf/daili/yy.php?id=1351762426
-Y Y 29,http://fogang136.cf/daili/yy.php?id=1331053117
-Y Y 30,http://fogang136.cf/daili/yy.php?id=1382660045
-Y Y 31,http://fogang136.cf/daili/yy.php?id=29216766
-Y Y 32,http://fogang136.cf/daili/yy.php?id=22039119
-Y Y 33,http://fogang136.cf/daili/yy.php?id=37228835
-Y Y 34,http://fogang136.cf/daili/yy.php?id=49259936
-Y Y 35,http://fogang136.cf/daili/yy.php?id=1353059120
-Y Y 36,http://fogang136.cf/daili/yy.php?id=36221298
-Y Y 37,http://fogang136.cf/daili/yy.php?id=35467265
-Y Y 38,http://fogang136.cf/daili/yy.php?id=30918390
-Y Y 39,http://fogang136.cf/daili/yy.php?id=1352677541
-Y Y 40,http://fogang136.cf/daili/yy.php?id=1353685311
-Y Y 41,http://fogang136.cf/daili/yy.php?id=34460526
-Y Y 42,http://fogang136.cf/daili/yy.php?id=23206872
-Y Y 43,http://fogang136.cf/daili/yy.php?id=79049030
-Y Y 44,http://fogang136.cf/daili/yy.php?id=29958059
-Y Y 45,http://fogang136.cf/daili/yy.php?id=1353314294
-Y Y 46,http://fogang136.cf/daili/yy.php?id=1351505899
-Y Y 47,http://fogang136.cf/daili/yy.php?id=23120006
-Y Y 48,http://fogang136.cf/daili/yy.php?id=35133142
-Y Y 49,http://fogang136.cf/daili/yy.php?id=37988782
-Y Y 50,http://fogang136.cf/daili/yy.php?id=21090129
-Y Y 51,http://fogang136.cf/daili/yy.php?id=27772604
-Y Y 52,http://fogang136.cf/daili/yy.php?id=35285823
-Y Y 53,http://fogang136.cf/daili/yy.php?id=1393837072
-Y Y 54,http://fogang136.cf/daili/yy.php?id=31809939
-Y Y 55,http://fogang136.cf/daili/yy.php?id=25560981
-Y Y 56,http://fogang136.cf/daili/yy.php?id=23870569
-Y Y 57,http://fogang136.cf/daili/yy.php?id=28882933
-Y Y 58,http://fogang136.cf/daili/yy.php?id=28205255
-Y Y 59,http://fogang136.cf/daili/yy.php?id=1350998397
-Y Y 60,http://fogang136.cf/daili/yy.php?id=42932063
-Y Y 61,http://fogang136.cf/daili/yy.php?id=35325313
-Y Y 62,http://fogang136.cf/daili/yy.php?id=38650266
-Y Y 63,http://fogang136.cf/daili/yy.php?id=1352469735
-Y Y 64,http://fogang136.cf/daili/yy.php?id=1353312849
-Y Y 65,http://fogang136.cf/daili/yy.php?id=1353426319
-Y Y 66,http://fogang136.cf/daili/yy.php?id=32140089
-Y Y 67,http://fogang136.cf/daili/yy.php?id=30667538
-Y Y 68,http://fogang136.cf/daili/yy.php?id=1352227227
-Y Y 69,http://fogang136.cf/daili/yy.php?id=40915353
-Y Y 70,http://fogang136.cf/daili/yy.php?id=1353587611
-Y Y 71,http://fogang136.cf/daili/yy.php?id=1353177492
-Y Y 72,http://fogang136.cf/daili/yy.php?id=1353085648
-Y Y 73,http://fogang136.cf/daili/yy.php?id=1351074342
-Y Y 74,http://fogang136.cf/daili/yy.php?id=10425097
-Y Y 75,http://fogang136.cf/daili/yy.php?id=1350670730
-Y Y 76,http://fogang136.cf/daili/yy.php?id=37877178
-Y Y 77,http://fogang136.cf/daili/yy.php?id=36377960
-Y Y 78,http://fogang136.cf/daili/yy.php?id=30804490
-Y Y 79,http://fogang136.cf/daili/yy.php?id=36949900
-Y Y 80,http://fogang136.cf/daili/yy.php?id=23681346
-Y Y 81,http://fogang136.cf/daili/yy.php?id=79382500
-Y Y 82,http://fogang136.cf/daili/yy.php?id=1352965544
-Y Y 83,http://fogang136.cf/daili/yy.php?id=39204655
-Y Y 84,http://fogang136.cf/daili/yy.php?id=1352216562
-Y Y 85,http://fogang136.cf/daili/yy.php?id=24686090
-Y Y 86,http://fogang136.cf/daili/yy.php?id=1352906405
-Y Y 87,http://fogang136.cf/daili/yy.php?id=1351428836
-Y Y 88,http://fogang136.cf/daili/yy.php?id=25519088
-Y Y 89,http://fogang136.cf/daili/yy.php?id=27289502
-Y Y 90,http://fogang136.cf/daili/yy.php?id=28099939
-Y Y 91,http://fogang136.cf/daili/yy.php?id=1331686180
-Y Y 92,http://fogang136.cf/daili/yy.php?id=1352356194
-Y Y 93,http://fogang136.cf/daili/yy.php?id=31292828
-Y Y 94,http://fogang136.cf/daili/yy.php?id=1351844200
-Y Y 95,http://fogang136.cf/daili/yy.php?id=1352875014
-Y Y 96,http://fogang136.cf/daili/yy.php?id=95017956
-Y Y 97,http://fogang136.cf/daili/yy.php?id=1335509613
-Y Y 98,http://fogang136.cf/daili/yy.php?id=36912445
-Y Y 99,http://fogang136.cf/daili/yy.php?id=30510265
-Y Y 100,http://fogang136.cf/daili/yy.php?id=25055036
-Y Y 101,http://fogang136.cf/daili/yy.php?id=1350562684
-Y Y 102,http://fogang136.cf/daili/yy.php?id=1352475619
-Y Y 103,http://fogang136.cf/daili/yy.php?id=24407222
-Y Y 104,http://fogang136.cf/daili/yy.php?id=22059534
-Y Y 105,http://fogang136.cf/daili/yy.php?id=10425098
-Y Y 106,http://fogang136.cf/daili/yy.php?id=26870427
-Y Y 107,http://fogang136.cf/daili/yy.php?id=1353455515
-Y Y 108,http://fogang136.cf/daili/yy.php?id=1352487950
-Y Y 109,http://fogang136.cf/daili/yy.php?id=33453966
-Y Y 110,http://fogang136.cf/daili/yy.php?id=36171593
-Y Y 111,http://fogang136.cf/daili/yy.php?id=1353518742
-Y Y 112,http://fogang136.cf/daili/yy.php?id=1353098382
-Y Y 113,http://fogang136.cf/daili/yy.php?id=1351052509
-Y Y 114,http://fogang136.cf/daili/yy.php?id=26664495
-Y Y 115,http://fogang136.cf/daili/yy.php?id=1351066072
-Y Y 116,http://fogang136.cf/daili/yy.php?id=37708809
-Y Y 117,http://fogang136.cf/daili/yy.php?id=22777075
-Y Y 118,http://fogang136.cf/daili/yy.php?id=1351086880
-Y Y 119,http://fogang136.cf/daili/yy.php?id=69304980
-Y Y 120,http://fogang136.cf/daili/yy.php?id=1353103539
-Y Y 121,http://fogang136.cf/daili/yy.php?id=24208878
-Y Y 122,http://fogang136.cf/daili/yy.php?id=1353392400
-Y Y 123,http://fogang136.cf/daili/yy.php?id=1353594050
-Y Y 124,http://fogang136.cf/daili/yy.php?id=1349469149
-Y Y 125,http://fogang136.cf/daili/yy.php?id=31058047
-Y Y 126,http://fogang136.cf/daili/yy.php?id=1351459730
-Y Y 127,http://fogang136.cf/daili/yy.php?id=88229675
-Y Y 128,http://fogang136.cf/daili/yy.php?id=1351433593
-Y Y 129,http://fogang136.cf/daili/yy.php?id=135146134
 
 </body>
 </html>
